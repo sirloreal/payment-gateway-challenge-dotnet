88363e4e55cb510b4c0fe62de2e92cdff1b39da7
sirloreal <gary.page3010@gmail.com>
2025-04-03 18:10:59 +0100

    And put back the tests that AI deleted :D

--- test/PaymentGateway.Api.Tests/PaymentsControllerTests.cs
+++ test/PaymentGateway.Api.Tests/PaymentsControllerTests.cs
@@ -91,4 +91,56 @@ public class PaymentsControllerTests
         Assert.Equal(paymentRequest.Amount, actualPaymentResponse.Amount);
         Assert.Equal(paymentRequest.Currency, actualPaymentResponse.Currency);
     }
+
+    [Fact]
+    public async Task RetrievesAPaymentSuccessfully()
+    {
+        // Arrange
+        var payment = new PaymentResponse
+        {
+            Id = Guid.NewGuid(),
+            ExpiryYear = _random.Next(2023, 2030),
+            ExpiryMonth = _random.Next(1, 12),
+            Amount = _random.Next(1, 10000),
+            CardNumberLastFour = _random.Next(1111, 9999),
+            Currency = "GBP"
+        };
+
+        var paymentsRepository = new PaymentsRepository();
+        paymentsRepository.Add(payment);
+
+        var webApplicationFactory = new WebApplicationFactory<PaymentsController>();
+        var client = webApplicationFactory.WithWebHostBuilder(builder =>
+            builder.ConfigureServices(services => ((ServiceCollection)services)
+                .AddSingleton<IPaymentsRepository>(paymentsRepository)
+                .AddScoped<IPaymentsReadService, PaymentsReadService>()))
+            .CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"/api/Payments/{payment.Id}");
+        var paymentResponse = await response.Content.ReadFromJsonAsync<PaymentResponse>();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(paymentResponse);
+    }
+
+    [Fact]
+    public async Task Returns404IfPaymentNotFound()
+    {
+        // Arrange
+        var paymentsRepository = new PaymentsRepository();
+        var webApplicationFactory = new WebApplicationFactory<PaymentsController>();
+        var client = webApplicationFactory.WithWebHostBuilder(builder =>
+            builder.ConfigureServices(services => ((ServiceCollection)services)
+                .AddSingleton<IPaymentsRepository>(paymentsRepository)
+                .AddScoped<IPaymentsReadService, PaymentsReadService>()))
+            .CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"/api/Payments/{Guid.NewGuid()}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
 }
