5453ecf3add7a310d77c519434afe7537774cad0
sirloreal <gary.page3010@gmail.com>
2025-04-03 16:16:02 +0100

    Convert status enums to string values for response.

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -1,5 +1,6 @@
+using System.Text.Json.Serialization;
+
 using FluentValidation;
-using FluentValidation.AspNetCore;
 
 using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Services;
@@ -9,7 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 builder.Services.AddScoped<IValidator<PostPaymentRequest>, PostPaymentRequestValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
